acd4c7ae7663a09b355c761f5002536e48931f5c
Kenneth Rosas <kencrosas@gmail.com>
2021-11-02 16:50:54 -0400

    fixed error that resulted in the NEXT bike being queried instead of the one clicked on

--- about.js
+++ about.js
@@ -20,7 +20,7 @@ const html = `
         <img id="banner-image" src="/banner.jpg"> 
         </div>
   </header>
-        <div class='product-list'><h2>Welcome to the Kawasaki Motors Club</h2> <p>Here, you'll find a number of well known (some very much iconic) motorcycles from Kawasaki. At this moment, our club sells all products except the H2R which is reserved for only our professional motorists.</p><p>Personally, I have owned a 250R and the 650R. They are both beginner friendly and are incredibly forgiving. Accidentally dropping your cluth on either WONT cause you to do a wheelie and although it's quite limited on the top end, its acceleration is still very good for a motorcycle.</p></div>
+        <div class='product-list'><h2>Welcome to the Kawasaki Motors Club</h2> <p>Here, you'll find a number of well known (some very much iconic) motorcycles from Kawasaki. At this moment, our club sells all products except the H2R which is reserved for only our professional motorists.</p><p>Personally, I have owned a 250R and the 650R. They are both beginner friendly and are incredibly forgiving. Accidentally dropping your clutch on either WONT cause you to do a wheelie and although it's quite limited on the top end, its acceleration is still very good for a motorcycle.</p></div>
     </body>
 </html>`;
 
